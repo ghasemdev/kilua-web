f3876cfcc3ed0a83f641515ac7052b914b0ce3d2
ghasem.shirdel <shirdelghasem79@gmail.com>
2025-01-20 23:27:36 +0330

    feat: add click, double click and long click

--- src/commonMain/kotlin/dev/kilua/compose/foundation/layout/Box.kt
+++ src/commonMain/kotlin/dev/kilua/compose/foundation/layout/Box.kt
@@ -1,7 +1,6 @@
 package dev.kilua.compose.foundation.layout
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.*
 import dev.kilua.compose.style.toClassName
 import dev.kilua.compose.ui.Alignment
 import dev.kilua.compose.ui.Modifier
@@ -11,6 +10,10 @@ import dev.kilua.core.IComponent
 import dev.kilua.html.IDiv
 import dev.kilua.html.div
 import dev.kilua.utils.rem
+import web.dom.TouchEvent
+import web.dom.events.Event
+import web.dom.events.MouseEvent
+import web.window
 
 @LayoutScopeMarker
 @Immutable // TODO: Remove annotation after upstream fix
@@ -44,19 +47,82 @@ fun IComponent.Box(
     }
 }
 
+private const val LONG_CLICK_DURATION = 500 // ms
+private const val DOUBLE_CLICK_DELAY = 300 // ms
+
 @Composable
 fun IComponent.Box(
     modifier: Modifier = Modifier,
     contentAlignment: Alignment = Alignment.TopStart,
     onClick: () -> Unit,
+    onDoubleClick: (() -> Unit)? = null,
+    onLongClick: (() -> Unit)? = null,
     content: @Composable BoxScope.() -> Unit = {}
 ) {
+    var singleClickTimer by remember { mutableStateOf<Int?>(null) }
+    var longClickTimer by remember { mutableStateOf<Int?>(null) }
+    var clickCount by remember { mutableStateOf(0) }
+    var isLongClickTriggered by remember { mutableStateOf(false) }
+
+    // Long Click with Mouse and Touch
+    val startEvent: (Event) -> Unit = remember {
+        {
+            isLongClickTriggered = false
+
+            longClickTimer = window.setTimeout({
+                isLongClickTriggered = true
+                onLongClick?.invoke()
+                clickCount = 0
+                null
+            }, LONG_CLICK_DURATION)
+        }
+    }
+
+    val endEvent: (Event) -> Unit = remember {
+        {
+            longClickTimer?.let { window.clearTimeout(it) }
+            longClickTimer = null
+        }
+    }
+
+
     Div(
         modifier = modifier
             .classNames("ripple")
             .boxClasses(contentAlignment)
     ) {
-        onClick { onClick() }
+        // Mouse Events
+        onEvent<MouseEvent>("mousedown", startEvent)
+        onEvent<MouseEvent>("mouseup", endEvent)
+        onEvent<MouseEvent>("mouseleave", endEvent)
+
+        // Touch Events
+        onEvent<TouchEvent>("touchstart", startEvent)
+        onEvent<TouchEvent>("touchend", endEvent)
+        onEvent<TouchEvent>("touchcancel", endEvent)
+
+        // Click and Double Click
+        onEvent<MouseEvent>("click") {
+            if (isLongClickTriggered) {
+                return@onEvent
+            }
+
+            clickCount++
+            if (clickCount == 1) {
+                singleClickTimer = window.setTimeout({
+                    if (clickCount == 1) {
+                        onClick()
+                    }
+                    clickCount = 0
+                    null
+                }, DOUBLE_CLICK_DELAY)
+            } else if (clickCount == 2) {
+                singleClickTimer?.let { window.clearTimeout(it) }
+                clickCount = 0
+                onDoubleClick?.invoke()
+            }
+        }
+
         BoxScopeInstance.content()
     }
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -20,20 +20,11 @@
  * SOFTWARE.
  */
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import dev.kilua.Application
 import dev.kilua.Hot
 import dev.kilua.compose.ComposeModule
-import dev.kilua.compose.adaptive.Breakpoint
-import dev.kilua.compose.adaptive.TailwindcssBreakpoint
-import dev.kilua.compose.adaptive.WindowWidthSizeClass
-import dev.kilua.compose.adaptive.currentWindowAdaptiveInfo
-import dev.kilua.compose.adaptive.rememberBreakpoint
-import dev.kilua.compose.adaptive.rememberTailwindcssBreakpoint
+import dev.kilua.compose.adaptive.*
 import dev.kilua.compose.foundation.layout.Arrangement
 import dev.kilua.compose.foundation.layout.Box
 import dev.kilua.compose.foundation.layout.Column
@@ -58,11 +49,17 @@ class App : Application() {
             Column {
                 Box(
                     modifier = Modifier
-                        .width(100.px)
+                        .width(500.px)
                         .height(100.px)
-                        .background(Color.Red),
+                        .background(Color("#19273B")),
                     onClick = {
-                        text = "Hello world!"
+                        text = "onClick"
+                    },
+                    onDoubleClick = {
+                        text = "onDoubleClick"
+                    },
+                    onLongClick = {
+                        text = "onLongClick"
                     },
                 )
                 h1t(text)
