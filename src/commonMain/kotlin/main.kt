8bcff20cdfb942dee7dbb2fc262c4644ec41eac6
ghasem.shirdel <shirdelghasem79@gmail.com>
2025-01-25 23:07:40 +0330

    fix: issue to run

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -20,9 +20,6 @@
  * SOFTWARE.
  */
 
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import app.softwork.routingcompose.HashRouter
@@ -30,18 +27,8 @@ import app.softwork.routingcompose.Router
 import dev.kilua.Application
 import dev.kilua.Hot
 import dev.kilua.compose.ComposeModule
-import dev.kilua.compose.adaptive.Breakpoint
-import dev.kilua.compose.adaptive.TailwindcssBreakpoint
-import dev.kilua.compose.adaptive.WindowWidthSizeClass
-import dev.kilua.compose.adaptive.currentWindowAdaptiveInfo
-import dev.kilua.compose.adaptive.rememberBreakpoint
-import dev.kilua.compose.adaptive.rememberOrientation
-import dev.kilua.compose.adaptive.rememberTailwindcssBreakpoint
-import dev.kilua.compose.foundation.layout.Arrangement
-import dev.kilua.compose.foundation.layout.Box
-import dev.kilua.compose.foundation.layout.Column
-import dev.kilua.compose.foundation.layout.Row
-import dev.kilua.compose.foundation.layout.box
+import dev.kilua.compose.adaptive.*
+import dev.kilua.compose.foundation.layout.*
 import dev.kilua.compose.root
 import dev.kilua.compose.ui.Alignment
 import dev.kilua.compose.ui.Modifier
@@ -50,35 +37,9 @@ import dev.kilua.compose.ui.modifiers.classNames
 import dev.kilua.compose.ui.modifiers.height
 import dev.kilua.compose.ui.modifiers.width
 import dev.kilua.core.IComponent
-import dev.kilua.html.Color
-import dev.kilua.html.button
-import dev.kilua.html.divt
+import dev.kilua.html.*
 import dev.kilua.html.helpers.TagStyleFun.Companion.background
-import dev.kilua.html.px
-import dev.kilua.html.vh
-import dev.kilua.html.vw
 import dev.kilua.startApplication
-
-@Composable
-fun IComponent.AnimatedContent(
-    targetState: String,
-    content: @Composable (String) -> Unit
-) {
-    val transitionState = updateTransition(targetState, label = "page-transition")
-
-    val alpha by transitionState.animateFloat(
-        transitionSpec = { tween(durationMillis = 1000) },
-        label = "alpha"
-    ) { if (it == targetState) 1f else 0f }
-
-    box {
-        opacity(alpha.toDouble())
-        width(100.vw)
-        height(100.vh)
-
-        content(targetState)
-    }
-}
 
 class App : Application() {
     override fun start() {
@@ -86,41 +47,39 @@ class App : Application() {
             HashRouter(initPath = "/login") {
                 val router = Router.current
 
-                    AnimatedContent(router.currentPath.path) {
-                        route("/login") {
-                            box(contentAlignment = Alignment.Center) {
-                                width(100.vw)
-                                height(100.vh)
-                                background(color = Color.Red)
+                route("/login") {
+                    box(contentAlignment = Alignment.Center) {
+                        width(100.vw)
+                        height(100.vh)
+                        background(color = Color.Red)
 
-                                button {
-                                    onClick {
-                                        router.navigate("/home", hide = true, replace = true)
-                                    }
-
-                                    +"go to dashboard"
-                                }
+                        button {
+                            onClick {
+                                router.navigate("/home", hide = true, replace = true)
                             }
+
+                            +"go to dashboard"
                         }
-                        route("/home") {
-                            box(contentAlignment = Alignment.Center) {
-                                width(100.vw)
-                                height(100.vh)
-                                background(color = Color.Blue)
+                    }
+                }
+                route("/home") {
+                    box(contentAlignment = Alignment.Center) {
+                        width(100.vw)
+                        height(100.vh)
+                        background(color = Color.Blue)
 
-                                button {
-                                    onClick {
-                                        router.navigate("/login", hide = true, replace = true)
-                                    }
-
-                                    +"go to login"
-                                }
+                        button {
+                            onClick {
+                                router.navigate("/login", hide = true, replace = true)
                             }
+
+                            +"go to login"
                         }
                     }
                 }
             }
         }
+    }
 
     @Composable
     private fun IComponent.Clickable() {
